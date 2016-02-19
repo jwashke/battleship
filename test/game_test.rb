c68a54e2b20676ed486b00170c211fea93779052
Josh Washke <jwashke@icloud.com>
2016-02-19 08:35:49 -0700

    finished as its going to get this morning

--- test/game_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'test_helper'
-require_relative '../lib/battleship/game'
-
-class GameTest < Minitest::Test
-  def setup
-    @game_board  = Battleship::GameBoard.new
-    @test_helper = TestHelper.new
-  end
-end
